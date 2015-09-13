cbbb3386806fde5fedc1d3a5d7d441bb31bba23c
Devin <devin@blackhat.co.za>
2015-09-13 14:38:59 +0200

    added auth

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,3 +17,12 @@ Route::get('/', function () {
 Route::get('/home', 'HomeController@index');
 
 Route::resource('particle-user', 'ParticleUserController');
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
